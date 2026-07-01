a02609dd79d2074ea7dd49e7a580de08b5b3435f
petersonbabs <timiebabs@gmail.com>
2026-07-01 12:22:38 +0100

    dev

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Lesson360 - Turn Screen Time Into Learning Time',
   description: 'Educational platform for African families and learners. Structured learning by level/class with assessments and support.',
-  
 };
+
 
 export default function RootLayout({
   children,
